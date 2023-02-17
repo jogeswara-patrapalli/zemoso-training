9fa11214b4c25ce39002d7f69429f6dde62248e5
Jogeswara-patrapalli <jogeswara.patrapalli@zemosolabs.com>
2023-02-17 14:26:06 +0530

    js_assignment3_es6

---
+++ js_assignment3_es6/es6-assignment1.js
@@ -0,0 +1,16 @@
+// Refactor the following function into a one-liner:
+// const printName = (name) => {
+//                      return “Hi” + name;
+//            }
+
+//using template string literals
+const printName = (name) => `Hi ${name}`;
+console.log(printName('Jogi'));
+
+
+
+
+
+
+
+  
---
+++ js_assignment3_es6/es6-assignment2.js
@@ -0,0 +1,7 @@
+// Rewrite the following code using template literals
+// const printBill = (name, bill) => {
+//                      return “Hi “ + name + “, please pay: “ + bill;
+//            }
+
+const printBill = (name, bill) => `Hi  ${name}, please pay: ${bill}`;
+console.log(printBill('Jogi',1000));                     
